e56f890936f58b4a5cb612163160b6fcf36d498f
Vũ Đức Vượng <59680260+Vtearit@users.noreply.github.com>
2021-02-09 17:09:34 +0700

    2nd update - 09/02/2021

--- README.md
+++ README.md
@@ -1,15 +1,39 @@
 # MedyPlus FE | VuDucVuong | AHT Training
-# vtearit.github.io
 
-"Update 06/02/2021: 
-    - Edit Slider: Non-use Owl Carousel Lib
-    - Top-Selling section"
+![image](https://user-images.githubusercontent.com/59680260/107345985-c08e9180-6af6-11eb-8910-82d9a33af225.png)
 
-"Update 08/02/2021:
-    - Edit News Section, Newsletter Section and Brand Section
-    - Add Zoom and Rotate Animation
-    - Edit image file type"
+#
 
-"Update 09/02/2021:
-    - Edit Footer Section
-    - Responsive all media devices"
+## Update Detail
+
+### *Update 06/02/2021:*
+
+```sh
+
+- Edit Slider: Non-use Owl Carousel Lib
+
+- Top-Selling section
+
+```
+
+### Update 08/02/2021:
+
+```sh
+
+- Edit News Section, Newsletter Section and Brand Section
+
+- Add Zoom and Rotate Animation
+
+- Edit image file type
+
+```
+
+### Update 09/02/2021:
+
+```sh
+
+- Edit Footer Section
+
+- Responsive all media devices
+
+```
--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,6 @@
     });
 
     /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
-    });
-
-    /*------------------
 		Navigation
 	--------------------*/
     $(".mobile-menu").slicknav({
@@ -67,7 +59,6 @@
     /*------------------
        Top Selling
     --------------------*/
-
     var items=document.querySelector('.ts__items').children;
     var nextItems=document.querySelector(".ts-right");
     var prevItems=document.querySelector(".ts-left");
@@ -88,8 +79,7 @@
         if(ind==totalItems){
             ind=0;
         }
-    } 
-    else{
+    } else{
             if(ind==0){
                 ind=totalItems-1;
             }
@@ -101,33 +91,11 @@
     for(i=0;i<items.length;i++){
         items[i].classList.remove("active");
     }
+    
     items[ind].classList.add("active");     
 
     }
 
-    /*------------------
-       Brand Carousel
-    --------------------*/
-    // $(".logo-carousel").owlCarousel({
-    //     loop: false,
-    //     margin: 30,
-    //     nav: false,
-    //     items: 5,
-    //     dots: false,
-    //     navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
-    //     smartSpeed: 1200,
-    //     autoHeight: false,
-    //     mouseDrag: false,
-    //     autoplay: true,
-    //     responsive: {
-    //         0: {
-    //             items: 3,
-    //         },
-    //         768: {
-    //             items: 5,
-    //         }
-    //     }
-    // });
     /*------------------
         CountDown
     --------------------*/
